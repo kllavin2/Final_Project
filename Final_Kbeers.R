795ce2c1b9f4ba00c325a20ba6486d9bc0e491ad
Kristi <Kristi@KLL-PC>
2018-12-07 16:56:49 -0500

    get the data from GEO

--- Final_Kbeers.R
+++ Final_Kbeers.R
@@ -1,7 +1,13 @@
 #Get the data set for Reference series GSE3292
 #NCBI GDS1667
+#annotation file GPL570
+library(Biobase)
 library(GEOquery)
 gds <- getGEO(filename = "GDS1667_full.soft.gz")
+# get annotation file
+gpl <- getGEO(filename = "GPL570.annot")
+# Transform the object into an expression set object
+
 
 
 
