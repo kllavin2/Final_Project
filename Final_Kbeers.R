eca37337757224cd6320bbb1f6714dbf3d3e755a
Kristi <Kristi@KLL-PC>
2018-12-07 16:15:47 -0500

    get data

--- Final_Kbeers.R
+++ Final_Kbeers.R
@@ -1,5 +1,7 @@
 #Get the data set for Reference series GSE3292
 #NCBI GDS1667
 library(GEOquery)
-getGEOSuppFiles("GSE3292")
-gsd <- getGEO("GSE3292", GSEMatrix = TRUE)
+gds <- getGEO(filename = "GDS1667_full.soft.gz")
+
+
+
